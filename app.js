deebb96dacc723f5abbaaf73abcfec607ebb3fc8
TuanDo <dodangtuan609@example.com>
2025-11-18 00:36:38 +0700

    delete pino log

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(pinoHttp());
 
 // Healthcheck
 app.get('/health', (_req, res) => res.json({ ok: true }));
