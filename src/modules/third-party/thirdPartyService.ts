d7f24591dccd2be3d21c0a89e4432fdccb5700eb
TuanDo <dodangtuan609@example.com>
2025-12-20 01:04:04 +0700

    fix deploy

--- src/modules/third-party/thirdPartyService.ts
+++ src/modules/third-party/thirdPartyService.ts
@@ -9,26 +9,22 @@ const analyzeUrl = baseUrl + '/analyze';
 const captionUrl = baseUrl + '/caption';
 
 export const ThirdPartyService = {
-  async gen3DFromImage(file: Express.Multer.File): Promise<Buffer> {
+  async gen3DFromImage(file: Express.Multer.File): Promise<File> {
     const form = new FormData();
-
-    form.append("image", file.buffer, {
+    form.append('image', file.buffer, {
       filename: file.originalname,
       contentType: file.mimetype,
     });
-
     const res = await axios.post(gen3DUrl, form, {
       headers: form.getHeaders(),
-      responseType: "arraybuffer", // ✅ ĐÚNG
     });
 
-    if (res && res.status === 200) {
-      return Buffer.from(res.data); // ✅ GLB buffer
+    if (res && isSuccessfulResponse(res)) {
+      const data: File = res.data;
+      return Promise.resolve(data);
     }
-
-    throw new Error("Failed to generate 3D model");
+    return Promise.reject('Failed to generate 3D model');
   },
-
 
   async isValidImage(file: Express.Multer.File): Promise<boolean> {
     const form = new FormData();
